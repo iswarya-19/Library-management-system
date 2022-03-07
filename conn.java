a2c2965b505c3e0e3b9c372e29e018f102781f21
iswarya-19 <94168019+iswarya-19@users.noreply.github.com>
2022-03-07 16:39:38 +0530

    Add files via upload

---
+++ conn.java
@@ -0,0 +1,21 @@
+import java.sql.*;  
+
+public class conn{
+    Connection c;
+    Statement s;
+    public conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql://localhost:3306/lms","root","");    
+            s =c.createStatement(); 
+           
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    } 
+    public static void main(String args[])
+    {
+        new conn();
+        System.out.println("Database Connected");
+    }  
+}  
